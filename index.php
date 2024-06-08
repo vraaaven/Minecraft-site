045446d5fffe0b29a9345b536c99ca30685ef17c
vraaaven <amelnikov311@gmail.com>
2024-06-08 11:21:43 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="/components/header/css/styles.css">
+    <link rel="stylesheet" href="/components/footer/css/styles.css">
+    <link rel="stylesheet" href="/components/main/css/styles.css">
+    <link rel="stylesheet" href="default.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+          rel="stylesheet">
+</head>
+<body>
+<div class="header">
+    <div class="header__logo-container">
+        <img src="" alt="">
+    </div>
+    <nav class="header__container">
+        <ul class="header__menu">
+            <li class="header__menu-item">
+                <a href="" class="header__menu-link">Главная</a>
+            </li>
+            <li class="header__menu-item">
+                <a href="" class="header__menu-link">Новости</a>
+            </li>
+            <li class="header__menu-item">
+                <a href="" class="header__menu-link">Гайды</a>
+            </li>
+            <li class="header__menu-item">
+                <a href="" class="header__menu-link">Правила</a>
+            </li>
+            <li class="header__menu-item">
+                <a href="" class="header__menu-link">Контакты</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="header__socials">
+        <div class="header__socials-item">
+            <div class="header__socials-icon">
+                <img src="/components/header/images/twitch.png" alt="ХУЙ">
+            </div>
+            <a href="" class="header__socials-link">OleksaGompers</a>
+        </div>
+        <div class="header__socials-item">
+            <div class="header__socials-icon">
+                <img src="/components/header/images/twitch.png" alt="">
+            </div>
+            <a href="" class="header__socials-link">vraaaaven</a>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="main">
+        <div class="main__banner">
+            <img src="/components/main/images/24122036_08084d7ee2.jpeg" alt="" class="main_banner-image">
+            <div class="main__banner-text-container">
+                <h1 class="main__banner-headline">FIUGHSAIGFIUSGUFI</h1>
+                <div class="main__banner-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem dolores et fuga molestiae obcaecati?
+                    Adipisci aliquid animi nobis ratione repudiandae! Autem ducimus ipsum molestias totam. Dolor
+                    doloremque explicabo natus similique?
+                </div>
+                <a href="" class="main__banner-reg">Регистрация</a>
+            </div>
+        </div>
+        <div class="main__news-slider">
+            <a href="" class="main__news-headline">
+                <h2>Новости</h2>
+            </a>
+            <div class="main__slider">
+                <div class="main__slider-item">
+                    <a href="" class="main__slider-link">
+                        <img src="/components/main/images/photo_2024-04-16_11-35-40.jpg" alt=""
+                             class="main__slider-img">
+                        <div class="main__slider-text">НОВОСТЬ ДАВАЙ УРА</div>
+                        <div class="main__slider-date">11.11.2024</div>
+                    </a>
+                </div>
+                <div class="main__slider-item">
+                    <a href="" class="main__slider-link">
+                        <img src="/components/main/images/photo_2024-04-16_11-35-40.jpg" alt=""
+                             class="main__slider-img">
+                        <div class="main__slider-text">НОВОСТЬ ДАВАЙ УРА</div>
+                        <div class="main__slider-date">11.11.2024</div>
+                    </a>
+                </div>
+                <div class="main__slider-item">
+                    <a href="" class="main__slider-link">
+                        <img src="/components/main/images/photo_2024-04-16_11-35-40.jpg" alt="" class="main__slider-img">
+                        <div class="main__slider-text">НОВОСТЬ ДАВАЙ УРА</div>
+                        <div class="main__slider-date">11.11.2024</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+<div class="footer">
+    <div class="footer__item">
+        <div class="footer__menu-container">
+            <div class="footer__headline">Ссылки</div>
+            <ul class="footer__menu">
+                <li class="footer__menu-item">
+                    <a href="" class="footer__menu-link">Главная</a>
+                </li>
+                <li class="footer__menu-item">
+                    <a href="" class="footer__menu-link">Новости</a>
+                </li>
+                <li class="footer__menu-item">
+                    <a href="" class="footer__menu-link">Гайды</a>
+                </li>
+                <li class="footer__menu-item">
+                    <a href="" class="footer__menu-link">Правила</a>
+                </li>
+                <li class="footer__menu-item">
+                    <a href="" class="footer__menu-link">Контакты</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer__item">
+        <div class="footer__history">
+            <div class="footer__headline">
+                Наша история
+            </div>
+            <div class="footer__history-content">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad aliquam architecto culpa deserunt dolorem
+                dolorum earum enim et fuga libero neque possimus quibusdam, reiciendis repellendus, repudiandae unde vel
+                veritatis.
+            </div>
+        </div>
+    </div>
+    <div class="footer__item">
+        <div class="footer__socials">
+            <div class="footer__headline">Соцсети</div>
+            <div class="footer__socials-links">
+                <a href="" class="footer__socials-link">
+                    <img src="/components/header/images/twitch.png" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
